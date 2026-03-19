a9ad44b096f441f6c7767f0d50edd1f23e4678f4
jaauger <jaauger@yahoo.com>
2026-03-18 21:06:57 -0600

    Another github update

--- docs/assets/main-CPENaylb.js
+++
@@ -1,1 +0,0 @@
-import{b as o,P as a,l as n,u as s}from"./ProductList--CHG_27m.js";const i=new a("tents"),t=document.querySelector(".product-list");t&&new o("tents",i,t).init();async function e(){await n(),s()}e();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -69,16 +69,18 @@ export async function loadTemplate(path){
   return template;
 }
 
-export async function loadHeaderFooter(){
+export async function loadHeaderFooter() {
 
-  const headerTemplate = await loadTemplate("./partials/header.html");
+  const base = import.meta.env.BASE_URL;
+
+  const headerTemplate = await loadTemplate(`${base}partials/header.html`);
   const headerElement = document.querySelector("#main-header");
 
-  const footerTemplate = await loadTemplate("./partials/footer.html");
+  const footerTemplate = await loadTemplate(`${base}partials/footer.html`);
   const footerElement = document.querySelector("#main-footer");
 
   renderWithTemplate(headerTemplate, headerElement);
-  renderWithTemplate(footerTemplate, footerElement)
+  renderWithTemplate(footerTemplate, footerElement);
 }
 
 export function updateCartCount() {
