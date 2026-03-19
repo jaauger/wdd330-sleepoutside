e95485202249e784b531e2a6bf4f7e82003b7182
jaauger <jaauger@yahoo.com>
2026-03-18 20:17:20 -0600

    github update in vite.config

---
+++ docs/assets/main-DbrKXUYa.js
@@ -0,0 +1,1 @@
+import{b as o,P as a,l as n,u as s}from"./ProductList-DD7C0efa.js";const i=new a("tents"),t=document.querySelector(".product-list");t&&new o("tents",i,t).init();async function e(){await n(),s()}e();
